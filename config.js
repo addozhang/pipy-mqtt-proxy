271ff3e8c810fa09cf69a3e5d1d5d00d19002651
Addo.Zhang <duwasai@gmail.com>
2024-05-24 18:02:50 +0800

    update connection quota logic
    Signed-off-by: Addo.Zhang <duwasai@gmail.com>

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ var brokerCapacities = config.brokers.reduce(function (caps, i) {
 var connRate = Number.parseInt(config.limits.conn.rate)
 var connQuota = new algo.Quota((connRate < brokerCapacities ? connRate : brokerCapacities) / __thread.concurrency)
 
-// invalid a broker
+// valid a broker
 //   1. add to unhealthy cache
 //   2. decrease connection quota
 var validBroker = (target) => {
@@ -21,18 +21,32 @@ var validBroker = (target) => {
   if (unhealthyBrokers.get(target.addr)) {
     unhealthyBrokers.remove(target.addr)
   }
-  connQuota.produce(target.capicity)
-  logger.log(`Broker ${target.addr} invalid, change connection quota from ${previous} to ${connQuota.current} `)
+  var pre = brokerCapacities
+  brokerCapacities += target.capicity
+  if (connRate > pre) {
+    if (connRate < brokerCapacities) {
+      connQuota.produce((connRate - pre) / __thread.concurrency)
+    } else {
+      connQuota.produce(target.capicity / __thread.concurrency)
+    }
+  }
+  logger.log(`Broker ${target.addr} valid, change connection quota from ${previous * __thread.concurrency} to ${connQuota.current * __thread.concurrency} `)
 }
 
-// valid a broker:
+// invalid a broker:
 //   1. remove from unhealthy cache
 //   2. increate connection quota
 var invalidBroker = (target) => {
   var previous = connQuota.current
   unhealthyBrokers.set(target.addr, true)
-  connQuota.consume(target.capicity)
-  logger.log(`Broker ${target.addr} invalid, change connection quota from ${previous} to ${connQuota.current} `)
+  var pre = brokerCapacities
+  brokerCapacities -= target.capicity
+  if (connRate < pre && connRate > brokerCapacities) {
+    connQuota.consume((connRate - brokerCapacities) / __thread.concurrency)
+  } else if (connRate >= pre) {
+    connQuota.consume(target.capicity / __thread.concurrency)
+  }
+  logger.log(`Broker ${target.addr} invalid, change connection quota from ${previous * __thread.concurrency} to ${connQuota.current * __thread.concurrency} `)
 }
 
 export { config, logger, unhealthyBrokers, connQuota, validBroker, invalidBroker }
